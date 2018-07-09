fcad365fa3318398dce79804728c428ec2ab22e0
camusicjunkie <33888807+camusicjunkie@users.noreply.github.com>
2018-07-09 17:01:18 -0600

    Update PowerStig.psm1

--- PowerStig.psm1
+++ PowerStig.psm1
@@ -1,6 +1,7 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+using module .\Public\Class\Common.Enum.psm1
 using module .\Public\Class\Stig.StigData.psm1
 using module .\Public\Class\Stig.TechnologyRole.psm1
 using module .\Public\Class\Stig.TechnologyVersion.psm1
