cc935fa8602ee7ac0def32efb6173ce2d324ec90
Adam Haynes <adamhay@microsoft.com>
2018-08-09 15:32:14 +0200

    added integration tests

---
+++ Tests/Integration/DSCResources/WindowsDnsServer.config.ps1
@@ -0,0 +1,34 @@
+Configuration WindowsDnsServer_config
+{
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [string]
+        $OsVersion,
+
+        [Parameter(Mandatory = $true)]
+        [version]
+        $StigVersion,
+
+        [Parameter(Mandatory = $true)]
+        [string]
+        $ForestName,
+
+        [Parameter(Mandatory = $true)]
+        [string]
+        $DomainName
+    )
+
+    Import-DscResource -ModuleName PowerStig
+
+    Node localhost
+    {
+        WindowsDnsServer BaseLineSettings
+        {
+            OsVersion   = $OsVersion
+            StigVersion = $StigVersion
+            ForestName  = $ForestName
+            DomainName  = $DomainName
+        }
+    }
+}
