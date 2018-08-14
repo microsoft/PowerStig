9b0d5888ec743ef070fb61697c728c5f7e82b4c1
Adam Haynes <adamhay@microsoft.com>
2018-08-14 16:33:33 +0200

    rem duplicate module call

--- PowerStig.Document.psm1
+++ PowerStig.Document.psm1
@@ -1,7 +1,6 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-using module .\Module\Common\Common.psm1
 using module .\Module\Document.Main\Document.Main.psm1
 
 Export-ModuleMember -Function 'New-StigCheckList'
