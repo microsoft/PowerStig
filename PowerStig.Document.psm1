887972c921802c1bbeb5a27a3f64d78d56842854
Adam Haynes <adam.haynes@outlook.com>
2018-08-17 16:11:53 +0200

    Manifest fix (#80)
    * fixed document module load
    
    * added test
    
    * rem spaces
    
    * updated module test

--- PowerStig.Document.psm1
+++
@@ -1,6 +0,0 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
-
-using module .\Module\Document.Main\Document.Main.psm1
-
-Export-ModuleMember -Function 'New-StigCheckList'
