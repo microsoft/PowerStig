27d3ae777c2327a1ac5f2c9e54465bd0c9e47011
Adam Haynes <adamhay@microsoft.com>
2018-08-21 15:56:25 +0200

    updated paths

--- Tests/CompositeResourceFilter.psd1
+++
@@ -1,9 +0,0 @@
-# This hash table is used filter for applicable stig data for a specific composite resource
-@{
-    Browser          = @("*IE*")
-    DotNetFramework  = @("*DotNet4*")
-    SqlServer        = @("*Instance*", "*Database*")
-    WindowsFirewall  = @("*FW*")
-    WindowsDnsServer = @("*DNS*")
-    WindowsServer    = @("*DC*", "*MS*")
-}
--- Tests/Unit/Module/.tests.header.ps1
+++ Tests/Unit/Module/.tests.header.ps1
@@ -3,15 +3,9 @@ $script:moduleRoot = Split-Path -Parent (Split-Path -Parent (Split-Path -Parent 
 $script:moduleName = (Get-PSCallStack)[1].Command -replace '\.tests\.ps1', ''
 $script:modulePath = "$($script:moduleRoot)$(($PSScriptRoot -split 'Unit')[1])\$script:moduleName\$($script:moduleName).psm1"
 
-if ( (-not (Test-Path -Path (Join-Path -Path $script:moduleRoot -ChildPath 'PowerStig.Tests'))) -or `
-     (-not (Test-Path -Path (Join-Path -Path $script:moduleRoot -ChildPath 'PowerStig.Tests\TestHelper.psm1'))) )
-{
-    & git @('clone', 'https://github.com/Microsoft/PowerStig.Tests', (Join-Path -Path $script:moduleRoot -ChildPath 'PowerStig.Tests'))
-}
-
 Import-Module -Name (Join-Path -Path $script:moduleRoot -ChildPath (
-                     Join-Path -Path 'PowerStig.Tests'  -ChildPath 'TestHelper.psm1')
-                    ) -Force -Global
+                        Join-Path -Path 'Tests' -ChildPath 'TestHelper.psm1')
+                     ) -Force -Global
 
 <#
     Several classes check for duplicate rules against a global variable stigSettings.
