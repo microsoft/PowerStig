5493a972dff1c840ed51dc503759f3598ad3fd72
Adam Haynes <adamhay@microsoft.com>
2018-08-21 16:09:48 +0200

    added test data

--- Tests/Unit/Module/.tests.header.ps1
+++ Tests/Unit/Module/.tests.header.ps1
@@ -3,9 +3,7 @@ $script:moduleRoot = Split-Path -Parent (Split-Path -Parent (Split-Path -Parent 
 $script:moduleName = (Get-PSCallStack)[1].Command -replace '\.tests\.ps1', ''
 $script:modulePath = "$($script:moduleRoot)$(($PSScriptRoot -split 'Unit')[1])\$script:moduleName\$($script:moduleName).psm1"
 
-Import-Module -Name (Join-Path -Path $script:moduleRoot -ChildPath (
-                        Join-Path -Path 'Tests' -ChildPath 'TestHelper.psm1')
-                     ) -Force -Global
+Import-Module -Name (Join-Path -Path $script:moduleRoot -ChildPath 'Tools\TestHelper\TestHelper.psm1') -Force -Global
 
 <#
     Several classes check for duplicate rules against a global variable stigSettings.
--- Tools/TestHelper/TestHelper.psm1
+++ Tools/TestHelper/TestHelper.psm1
@@ -1,3 +1,252 @@
+
+<#
+    .SYNOPSIS
+        Used to process the Check-Content test strings the same way that the SplitCheckContent
+        static method does in the the STIG base class.
+    .PARAMETER CheckContent
+        Strings to process.
+#>
+function Split-TestStrings
+{
+    [OutputType([string[]])]
+    param
+    (
+        [Parameter(mandatory = $true)]
+        [string]
+        $CheckContent
+    )
+
+    $CheckContent -split '\n' |
+        Select-String -Pattern "\w" |
+            ForEach-Object { $PSitem.ToString().Trim() }
+}
+
+<#
+    .SYNOPSIS
+        Used to validate an xml file against a specified schema
+
+    .PARAMETER XmlFile
+        Path and file name of the XML file to be validated
+
+    .PARAMETER Xml
+        An already loaded System.Xml.XmlDocument
+
+    .PARAMETER SchemaFile
+        Path of XML schema used to validate the XML document
+
+    .PARAMETER ValidationEventHandler
+        Script block that is run when an error occurs while validating XML
+
+    .EXAMPLE
+        Test-XML -XmlFile C:\source\test.xml -SchemaFile C:\Source\test.xsd
+
+    .EXAMPLE
+        $xmlobject = Get-StigData -OsVersion 2012R2 -OsRole MemberServer
+        Test-XML -Xml $xmlobject -SchemaFile C:\Source\test.xsd
+#>
+function Test-Xml
+{
+    [OutputType([Boolean])]
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(ValueFromPipeline = $true, Mandatory = $true, ParameterSetName = 'File')]
+        [string]
+        $XmlFile,
+
+        [Parameter(ValueFromPipeline = $true, Mandatory = $true, ParameterSetName = 'Object')]
+        [xml]
+        $Xml,
+
+        [Parameter(Mandatory = $true)]
+        [string]
+        $SchemaFile,
+
+        [scriptblock]
+        $ValidationEventHandler = { Throw $PSItem.Exception }
+    )
+
+    If (-not (Test-Path -Path $SchemaFile))
+    {
+        Throw "Schema file not found"
+    }
+
+    $schemaReader = New-Object System.Xml.XmlTextReader $SchemaFile
+    $schema = [System.Xml.Schema.XmlSchema]::Read($schemaReader, $ValidationEventHandler)
+
+    If ($PsCmdlet.ParameterSetName -eq "File")
+    {
+        $xml = New-Object System.Xml.XmlDocument
+        $xml.Load($XmlFile)
+    }
+
+    $xml.Schemas.Add($schema) | Out-Null
+    $xml.Validate($ValidationEventHandler)
+}
+
+<#
+    .SYNOPSIS
+        Creates a sample xml docuement that injects class specifc data
+
+    .PARAMETER TestFile
+        The test rule to merge into the data
+#>
+function Get-TestStigRule
+{
+    [CmdletBinding(DefaultParameterSetName = 'UseExisting')]
+    param
+    (
+        [Parameter(Parametersetname = 'UseExisting')]
+        [string]
+        $XccdfTitle = '(Technology) Security Technical Implementation Guide',
+
+        [Parameter(Parametersetname = 'UseExisting')]
+        [string]
+        $XccdfRelease = 'Release: 1 Benchmark Date: 1 Jan 1970',
+
+        [Parameter(Parametersetname = 'UseExisting')]
+        [string]
+        $XccdfVersion = '2',
+
+        [Parameter(Parametersetname = 'UseExisting')]
+        [string]
+        $XccdfId = 'Technology_Target',
+
+        [Parameter(Parametersetname = 'UseExisting')]
+        [string]
+        $CheckContent = 'This is a string of text that tells an admin what item to check to verify compliance.',
+
+        [Parameter(Parametersetname = 'UseExisting')]
+        [string]
+        $GroupId = 'V-1000',
+
+        [Parameter(Parametersetname = 'UseExisting')]
+        [string]
+        $GroupTitle = 'Sample Title',
+
+        [Parameter(Parametersetname = 'UseExisting')]
+        [string]
+        $GroupRuleTitle = 'A more descriptive title.',
+
+        [Parameter(Parametersetname = 'UseExisting')]
+        [string]
+        $GroupRuleDescription = 'A description of what this vulnerability addresses and how it mitigates the threat.',
+
+        [Parameter(Parametersetname = 'UseExisting')]
+        [string]
+        $FixText = 'This is a string of text that tells an admin how to fix an item if it is not currently configured properly and ignored by the parser',
+
+        [Parameter(Parametersetname = 'UseExisting')]
+        [Parameter(Parametersetname = 'FileProvided')]
+        [switch]
+        $ReturnGroupOnly,
+
+        [Parameter(Mandatory = $true, Parametersetname = 'FileProvided')]
+        [string]
+        $FilePathToGroupElementText
+    )
+
+    # If a file path is provided, override the default sample group element text.
+    if ( $FilePathToGroupElementText )
+    {
+        try
+        {
+            $groupElement = Get-Content -Path $FilePathToGroupElementText -Raw
+        }
+        catch
+        {
+            throw "$FilePathToGroupElementText was not found"
+        }
+    }
+    else
+    {
+        # Get the samplegroup element text and merge in the parameter strings
+        $groupElement = Get-Content -Path "$PSScriptRoot\data\sampleGroup.xml.txt" -Encoding UTF8 -Raw
+        $groupElement = $groupElement -f $GroupId, $GroupTitle, $RuleTitle, $RuleDescription, $FixText, $CheckContent
+    }
+
+    # Get and merge the group element data into the xccdf xml document and create an xml object to return
+    $xmlDocument = Get-Content -Path "$PSScriptRoot\data\sampleXccdf.xml.txt" -Encoding UTF8 -Raw
+    [xml] $xmlDocument = $xmlDocument -f $XccdfTitle, $XccdfRelease, $XccdfVersion, $groupElement, $XccdfId
+
+    # Some tests only need the group to test functionality.
+    if ($ReturnGroupOnly)
+    {
+        return $xmlDocument.Benchmark.group
+    }
+
+    return $xmlDocument
+}
+
+<#
+    .SYNOPSIS
+        Used to retrieve an array of Stig class base methods and optionally add child class methods
+        for purpose of testing methods
+
+    .PARAMETER ChildClassMethodNames
+        An array of child class method to add to the class base methods
+
+    .EXAMPLE
+        $RegistryRuleClassMethods = Get-StigBaseMethods -ChildClassMethodsNames @('SetKey','SetName')
+#>
+Function Get-StigBaseMethods
+{
+    [OutputType([System.Collections.ArrayList])]
+    param
+    (
+        [Parameter()]
+        [system.array]
+        $ChildClassMethodNames,
+
+        [Parameter()]
+        [switch]
+        $Static
+    )
+
+    if ( $Static )
+    {
+        $stigClassMethodNames = @('Equals', 'new', 'ReferenceEquals', 'SplitCheckContent',
+            'GetRuleTypeMatchList', 'GetFixText')
+    }
+    else
+    {
+        $objectClassMethodNames = @('Equals', 'GetHashCode', 'GetType', 'ToString')
+        $stigClassMethodNames = @('Clone', 'IsDuplicateRule', 'SetDuplicateTitle', , 'SetStatus',
+            'SetIsNullOrEmpty', 'SetOrganizationValueRequired', 'GetOrganizationValueTestString',
+            'ConvertToHashTable', 'SetStigRuleResource', 'IsHardCoded', 'GetHardCodedString',
+            'IsHardCodedOrganizationValueTestString', 'GetHardCodedOrganizationValueTestString',
+            'IsExistingRule')
+
+        $stigClassMethodNames += $ObjectClassMethodNames
+    }
+
+    if ( $ChildClassMethodNames )
+    {
+        $stigClassMethodNames += $ChildClassMethodNames
+    }
+
+    return ( $stigClassMethodNames | Select-Object -Unique )
+}
+
+function Format-RuleText
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory = $true)]
+        [AllowEmptyString()]
+        [string[]]
+        $RuleText
+    )
+
+    $return = ($RuleText -split '\n' |
+            Select-String -Pattern "\w" |
+            ForEach-Object { $PSitem.ToString().Trim() })
+
+    return $return
+}
+
+
 function Get-RequiredStigDataVersion
 {
     [CmdletBinding()]
@@ -185,6 +434,11 @@ function Get-ValidStigVersionNumbers
 }
 
 Export-ModuleMember -Function @(
+    'Split-TestStrings',
+    'Test-Xml',
+    'Get-TestStigRule',
+    'Get-StigBaseMethods',
+    'Format-RuleText',
     'Get-PowerStigVersionFromManifest',
     'Get-StigVersionTable',
     'Get-ConfigurationName',
