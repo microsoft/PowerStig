289b951c523d036989429dca4cd1cb92b4326710
Adam Haynes <adamhay@microsoft.com>
2018-08-27 08:45:53 +0200

    removed unused files

--- Module/Stig.Main/Functions.ps1
+++ Module/Stig.Main/Functions.ps1
@@ -1,7 +1,8 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
-#region Main Functions
+#region Get-DomainName
+
 <#
     .SYNOPSIS
         Enforces the behavior of getting the domain name.
@@ -106,9 +107,6 @@ Function Get-DomainName
         }
     }
 }
-
-#endregion Main Functions
-#region Support Functions
 
 <#
  .SYNOPSIS
@@ -218,4 +216,84 @@ Function Get-DomainParts
 
     return $FQDN.Split('{.}')
 }
-#endregion Support Functions
+#endregion
+
+#region Get-OrgSettingsObject
+
+<#
+.SYNOPSIS
+    Creates an OrganizationSetting object
+
+.PARAMETER OrgSettings
+    OrgSettings can be either a string path to an OrganizationalFile XML or a hash table of org settings
+    to override from the default organization file.
+
+.RETURN
+    OrganizationalSetting
+
+.EXAMPLE
+    Get-OrgSettingsObject -OrgSettings @{"v-1000"="15"}
+#>
+Function Get-OrgSettingsObject
+{
+    [CmdletBinding()]
+    [OutputType([OrganizationalSetting])]
+    param
+    (
+        [Parameter(Mandatory = $True)]
+        [ValidateNotNullOrEmpty()]
+        [PSObject]
+        $OrgSettings
+    )
+
+    switch ($OrgSettings.GetType())
+    {
+        "string"
+        {
+            if (Test-Path -Path $OrgSettings)
+            {
+                [xml] $orgSettingsXml = Get-Content -Path $orgSettings
+                $orgSettingsObject = [OrganizationalSetting]::ConvertFrom($orgSettingsXml)
+            }
+            else
+            {
+                Throw "Organizational file not found"
+            }
+        }
+        "xml"
+        {
+            $orgSettingsObject = [OrganizationalSetting]::ConvertFrom($OrgSettings)
+        }
+        "hashtable"
+        {
+            $orgSettingsObject = [OrganizationalSetting]::ConvertFrom($OrgSettings)
+        }
+    }
+
+    return $orgSettingsObject
+}
+
+
+#endregion
+
+#region Get-StigList
+
+<#
+    .SYNOPSIS
+        Returns an array of all available STIGs with the associated Technology, TechnologyVersion, TechnologyRole, and StigVersion.
+        This function is a wrapper of the StigData class, and the return of this function call will provide you with the values needed
+        to create a default StigData object.
+
+    .EXAMPLE
+        Get-StigList
+#>
+Function Get-StigList
+{
+    [CmdletBinding()]
+    [OutputType([PSObject[]])]
+    param ()
+
+    return [StigData]::GetAvailableStigs()
+}
+
+#endregion
