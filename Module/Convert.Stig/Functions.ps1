3416497335faa3077862c55d017af4d10a4f9c94
Adam Haynes <adamhay@microsoft.com>
2018-08-30 16:33:23 +0200

    added hard coded value

--- Module/Convert.Stig/Functions.ps1
+++ Module/Convert.Stig/Functions.ps1
@@ -4,16 +4,24 @@
 #endregion
 #region Data
 # These are the registry strings that are not able to be automatically extracted from the xccdf.
-$script:legalNoticeText = "You are accessing a U.S. Government (USG) Information System (IS) that is provided for USG-authorized use only.
+$script:legalNoticeText = '
+You are accessing a U.S. Government (USG) Information System (IS) that is provided for USG-authorized use only.
+
 By using this IS (which includes any device attached to this IS), you consent to the following conditions:
+
 -The USG routinely intercepts and monitors communications on this IS for purposes including, but not limited to, penetration testing, COMSEC monitoring, network operations and defense, personnel misconduct (PM), law enforcement (LE), and counterintelligence (CI) investigations.
+
 -At any time, the USG may inspect and seize data stored on this IS.
+
 -Communications using, or data stored on, this IS are not private, are subject to routine monitoring, interception, and search, and may be disclosed or used for any USG-authorized purpose.
+
 -This IS includes security measures (e.g., authentication and access controls) to protect USG interests--not for your personal benefit or privacy.
--Notwithstanding the above, using this IS does not constitute consent to PM, LE or CI investigative searching or monitoring of the content of privileged communications, or work product, related to personal representation or services by attorneys, psychotherapists, or clergy, and their assistants. Such communications and work product are private and confidential. See User Agreement for details."
+
+-Notwithstanding the above, using this IS does not constitute consent to PM, LE or CI investigative searching or monitoring of the content of privileged communications, or work product, related to personal representation or services by attorneys, psychotherapists, or clergy, and their assistants.  Such communications and work product are private and confidential.  See User Agreement for details.'
 $script:legalNoticeCaption = 'DoD Notice and Consent Banner'
 $script:supportedEncryptionTypes = '0'
 $script:smb1FeatureName = 'FS-SMB1'
+$script:publishersCertificateRevocation = '146432'
 #endregion
 #region Main Functions
 <#
@@ -45,9 +53,11 @@ function Test-ValueDataIsHardCoded
         'V-26359', # Windows Server - Legal Banner Dialog Box Title
         'V-63681', # Windows Client - Legal Banner Dialog Box Title
         'V-21954', # Windows Server - Kerberos Supported Encryption Types
-        'V-73805'  # Windows Server - Disable SMB1 'V-70639' is on the client, but the WindowsFeature
+        'V-73805', # Windows Server - Disable SMB1 'V-70639' is on the client, but the WindowsFeature
         # resource does not use the Get-WindowsOptionalFeature cmdlet so this has to stay manual on
         # the desktop for now or else we have to move everything to a script resource.
+        'V-46477' # Internet Explorer - Publishers Certificate Revocation. The value s written as hex,
+        # but there is not other identifier.
     )
 
     if ($stigIds -contains $StigId)
@@ -68,8 +78,6 @@ function Test-ValueDataIsHardCoded
 
     .PARAMETER StigId
         The Stig ID to check for a fixed string
-
-    .NOTES
  #>
 function Get-HardCodedString
 {
@@ -87,33 +95,31 @@ function Get-HardCodedString
         {$PSItem -match 'V-(1089|63675)'}
         {
             Write-Verbose -Message "[$($MyInvocation.MyCommand.Name)] LegalNotice : $true"
-            $hardCodedString = $script:legalNoticeText
-            continue
+            return $script:legalNoticeText
         }
         {$PSItem -match 'V-(26359|63681)'}
         {
             Write-Verbose -Message "[$($MyInvocation.MyCommand.Name)] LegalCaption : $true"
-            $hardCodedString = $script:legalNoticeCaption
-            continue
+            return $script:legalNoticeCaption
         }
 
         {$PSItem -match 'V-(21954|30935)'}
         {
             Write-Verbose -Message "[$($MyInvocation.MyCommand.Name)] SupportedEncryptionTypes : $true"
-            $hardCodedString = $script:supportedEncryptionTypes
-            continue
+            return $script:supportedEncryptionTypes
         }
 
         {$PSItem -match 'V-73805'}
         {
-            Write-Verbose -Message "[$($MyInvocation.MyCommand.Name)] SMB1: $true"
-            $hardCodedString = $script:smb1FeatureName
-            continue
+            Write-Verbose -Message "[$($MyInvocation.MyCommand.Name)] SMB1 : $true"
+            return $script:smb1FeatureName
         }
-
+        {$PSItem -match 'V-46477'}
+        {
+            Write-Verbose -Message "[$($MyInvocation.MyCommand.Name)] PublishersCertificateRevocation : $true"
+            return $script:publishersCertificateRevocation
+        }
     }
-
-    return $hardCodedString
 }
 
 <#
