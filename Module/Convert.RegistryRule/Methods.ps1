59b00c97e6b3f46caf373f3620bd522d3b92828c
Chase Wilson <phwilson@microsoft.com>
2018-09-05 14:41:40 -0700

    update to fix bug

--- Module/Convert.RegistryRule/Methods.ps1
+++ Module/Convert.RegistryRule/Methods.ps1
@@ -132,10 +132,10 @@ function Get-RegistryPathFromWindowsStig
                 Write-Verbose -Message "[$($MyInvocation.MyCommand.Name)] Fixed Leading Backslash : $path"
             }
 
-            # There is an edge case where the STIG has a typo and the path is writen with a space after \SOFTWARE\  (V-68819)
-            if  ($path -match '\sP' )
+            # There is an edge case where the STIG has a typo and the path is written with a space after \SOFTWARE\  (V-68819)
+            if  ($path -match '\\\sP' )
             {
-                $path = $path -replace '\s'
+                $path = $path -replace '\\\sP', '\P'
             }
             $result += $path
         }
