88cc384d481dea9210dff16ac5525170726d4cb1
Chase Wilson <phwilson@microsoft.com>
2018-09-06 01:24:15 -0700

    update to fix 2 rules

--- Module/Convert.RegistryRule/Methods.ps1
+++ Module/Convert.RegistryRule/Methods.ps1
@@ -1002,7 +1002,6 @@ function Split-MultipleRegistryEntries
         {
             $paths = $paths -split " and "
         }
-
         foreach ($path in $paths)
         {
             if (![string]::IsNullOrWhiteSpace($path))
