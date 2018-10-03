b6063b9674ee6987d33844fd3f8b708459065265
Chase Wilson <phwilson@microsoft.com>
2018-10-03 15:56:38 -0700

    update for tests.

--- Tests/Unit/DSCResources/Composite.tests.ps1
+++ Tests/Unit/DSCResources/Composite.tests.ps1
@@ -87,7 +87,9 @@ Describe 'Common Tests - Configuration Module Requirements' {
             WindowsServer    = 'DC|MS'
             Office           = 'Outlook2013|Excel2013|PowerPoint2013|Word2013'
             WindowsClient    = 'Client'
+            FireFox          = 'FireFox'
         }
+
         Foreach ($resource in $moduleDscResourceList)
         {
             Context "$resource Composite Resource" {
