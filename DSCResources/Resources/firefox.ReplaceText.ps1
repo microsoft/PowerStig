4a23e4815500f759b5d3285c269a815c9fd52615
Chase Wilson <phwilson@microsoft.com>
2018-10-03 19:38:48 -0700

    update for parameter update

--- DSCResources/Resources/firefox.ReplaceText.ps1
+++ DSCResources/Resources/firefox.ReplaceText.ps1
@@ -7,21 +7,22 @@ if (-not(Test-Path -Path $InstallDirectory))
     throw "$InstallDirectory not found"
 }
 
-# add autoconfig.js with the required preferences
 ReplaceText GeneralConfigFileName
 {
-    Path   = "$InstallDirectory\defaults\pref\autoconfig.js"
-    Search = 'pref\("general.config.filename", (.*)\);'
-    Type   = 'Text'
-    Text   = 'pref("general.config.filename", "firefox.cfg");'
+    Path        = "$InstallDirectory\defaults\pref\autoconfig.js"
+    Search      = 'pref\("general.config.filename", (.*)\);'
+    Type        = 'Text'
+    Text        = 'pref("general.config.filename", "firefox.cfg");'
+    AllowAppend = $true
 }
 
 ReplaceText DoNotObscureFile
 {
-    Path   = "$InstallDirectory\defaults\pref\autoconfig.js"
-    Search = 'pref\("general.config.obscure_value", (.*)\);'
-    Type   = 'Text'
-    Text   = 'pref("general.config.obscure_value", 0);'
+    Path        = "$InstallDirectory\defaults\pref\autoconfig.js"
+    Search      = 'pref\("general.config.obscure_value", (.*)\);'
+    Type        = 'Text'
+    Text        = 'pref("general.config.obscure_value", 0);'
+    AllowAppend = $true
 }
 
 <#
@@ -30,19 +31,21 @@ ReplaceText DoNotObscureFile
 #>
 ReplaceText BeginFileWithComment
 {
-    Path   = "$InstallDirectory\firefox.cfg"
-    Search = '// FireFox preference file'
-    Type   = 'Text'
-    Text   = '// FireFox preference file'
+    Path        = "$InstallDirectory\firefox.cfg"
+    Search      = '// FireFox preference file'
+    Type        = 'Text'
+    Text        = '// FireFox preference file'
+    AllowAppend = $true
 }
 
 foreach ( $rule in $rules )
 {
     ReplaceText (Get-ResourceTitle -Rule $rule)
     {
-        Path   = "$InstallDirectory\FireFox.cfg"
-        Search = 'lockPref\("{0}", (.*)\);' -f $rule.Key
-        Type   = 'Text'
-        Text   = 'lockPref("{0}", {1});' -f $rule.Key, (Format-FireFoxPreference -Value $rule.Value)
+        Path        = "$InstallDirectory\FireFox.cfg"
+        Search      = 'lockPref\("{0}", (.*)\);' -f $rule.Key
+        Type        = 'Text'
+        Text        = 'lockPref("{0}", {1});' -f $rule.Key, (Format-FireFoxPreference -Value $rule.Value)
+        AllowAppend = $true
     }
 }
