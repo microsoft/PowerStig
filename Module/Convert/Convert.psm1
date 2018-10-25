94b3ff9ef18e0c875e917a23adee0cb663e03988
Adam Haynes <adamhay@microsoft.com>
2018-10-25 15:38:02 +0200

    rename ProcessMitigationRule module

--- Module/Convert/Convert.psm1
+++ Module/Convert/Convert.psm1
@@ -10,7 +10,7 @@ using module .\..\IISLoggingRule\IISLoggingRule.psm1
 using module .\..\ManualRule\ManualRule.psm1
 using module .\..\MimeTypeRule\MimeTypeRule.psm1
 using module .\..\PermissionRule\PermissionRule.psm1
-using module .\..\Convert.ProcessMitigationRule\Convert.ProcessMitigationRule.psm1
+using module .\..\ProcessMitigationRule\ProcessMitigationRule.psm1
 using module .\..\Convert.RegistryRule\Convert.RegistryRule.psm1
 using module .\..\Convert.SecurityOptionRule\Convert.SecurityOptionRule.psm1
 using module .\..\Convert.ServiceRule\Convert.ServiceRule.psm1
