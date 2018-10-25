aea307d8dfe9208e543d43f263c1caaa0684c19d
Adam Haynes <adamhay@microsoft.com>
2018-10-25 17:02:42 +0200

    renamed RegistryRule module

--- PowerStig.Convert.psm1
+++ PowerStig.Convert.psm1
@@ -23,7 +23,7 @@ using module .\Module\ManualRule\ManualRule.psm1
 using module .\Module\MimeTypeRule\MimeTypeRule.psm1
 using module .\Module\Convert.PermissionRule\Convert.PermissionRule.psm1
 using module .\Module\Convert.ProcessMitigationRule\Convert.ProcessMitigationRule.psm1
-using module .\Module\Convert.RegistryRule\Convert.RegistryRule.psm1
+using module .\Module\RegistryRule\RegistryRule.psm1
 using module .\Module\Convert.SecurityOptionRule\Convert.SecurityOptionRule.psm1
 using module .\Module\Convert.ServiceRule\Convert.ServiceRule.psm1
 using module .\Module\Convert.SqlScriptQueryRule\Convert.SqlScriptQueryRule.psm1
