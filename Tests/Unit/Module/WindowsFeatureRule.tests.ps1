dcc4f7592e1184dcb31aac67b6b09b0117e3c347
Adam Haynes <adamhay@microsoft.com>
2018-10-26 11:31:56 +0200

    Added unit test file

---
+++ Tests/Unit/Module/WindowsFeatureRule.tests.ps1
@@ -0,0 +1,46 @@
+#region Header
+using module .\..\..\..\Module\WindowsFeatureRule\WindowsFeatureRule.psm1
+. $PSScriptRoot\.tests.header.ps1
+#endregion
+try
+{
+    InModuleScope -ModuleName $script:moduleName {
+        #region Test Setup
+        $stigRule = Get-TestStigRule -ReturnGroupOnly
+        $rule = [WindowsFeatureRule]::new( $stigRule )
+        #endregion
+        #region Class Tests
+        Describe "$($rule.GetType().Name) Child Class" {
+
+            Context 'Base Class' {
+
+                It 'Shoud have a BaseType of STIG' {
+                    $rule.GetType().BaseType.ToString() | Should Be 'Rule'
+                }
+            }
+
+            Context 'Class Properties' {
+
+                $classProperties = @('FeatureName', 'InstallState')
+
+                foreach ( $property in $classProperties )
+                {
+                    It "Should have a property named '$property'" {
+                        ( $rule | Get-Member -Name $property ).Name | Should Be $property
+                    }
+                }
+            }
+        }
+        #endregion
+        #region Method Tests
+
+        #endregion
+        #region Data Tests
+
+        #endregion
+    }
+}
+finally
+{
+    . $PSScriptRoot\.tests.footer.ps1
+}
