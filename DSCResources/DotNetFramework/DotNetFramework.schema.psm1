76b3684f6e5c1b49a54f07ee4378417a7ba01a68
Adam Lichtenberg <adlichte@microsoft.com>
2018-11-16 09:16:44 -0600

    Updated ValidateSet for 1.6

--- DSCResources/DotNetFramework/DotNetFramework.schema.psm1
+++ DSCResources/DotNetFramework/DotNetFramework.schema.psm1
@@ -43,7 +43,7 @@ Configuration DotNetFramework
         $FrameworkVersion,
 
         [Parameter()]
-        [ValidateSet('1.4')]
+        [ValidateSet('1.4', '1.6')]
         [ValidateNotNullOrEmpty()]
         [version]
         $StigVersion,
