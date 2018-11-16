01f65338edec9840a1692d35dc1ead5468e688b5
La'Neice Lansey <LLansey@users.noreply.github.com>
2018-11-16 14:09:28 -0500

    Updating validate set and regex for Firefox 4.23 parsing.

--- Module/FileContentRule/Data.ps1
+++ Module/FileContentRule/Data.ps1
@@ -1,7 +1,7 @@
 data fileContentRegex
 {
     ConvertFrom-StringData -StringData @'
-        BetweenAllQuotes           = ({0}|"|{1})[\\s\\S]*?("|{0}|{1}|{2})
+        BetweenAllQuotes           = ("|{0}|{1}|{2})[\\s\\S]*?("|{0}|{1}|{2})
         RegexToRemove              = "|{0}|{1}|{2}
         TwoTo5CapitalLetters       = [A-Z]{2,5}
         CapitalsEndWithSpaceOrDot5 = [A-Z,1-9]{2,5}(\\s|\\.)
