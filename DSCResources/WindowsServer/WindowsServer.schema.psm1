05e2ce605c8a7331b793b04d210e9268795975fb
Adam Haynes <adamhay@microsoft.com>
2018-11-29 11:53:57 +0100

    updated param set

--- DSCResources/WindowsServer/WindowsServer.schema.psm1
+++ DSCResources/WindowsServer/WindowsServer.schema.psm1
@@ -62,7 +62,7 @@ Configuration WindowsServer
 
         [Parameter()]
         [ValidateNotNullOrEmpty()]
-        [ValidateSet('2.12', '2.13')]
+        [ValidateSet('2.12', '2.13', '2.14')]
         [version]
         $StigVersion,
 
