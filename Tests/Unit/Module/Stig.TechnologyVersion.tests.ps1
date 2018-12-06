8176611c1a59ffbdfaeff53ac5008c712fa96091
Adam Haynes <adamhay@microsoft.com>
2018-12-06 09:03:00 +0100

    added 2016

--- Tests/Unit/Module/Stig.TechnologyVersion.tests.ps1
+++ Tests/Unit/Module/Stig.TechnologyVersion.tests.ps1
@@ -16,7 +16,7 @@ try
         $Technology2 = [Technology]::SqlServer
 
         $TestValidateSet = @"
-Windows = All, 2012R2, 10
+Windows = All, 2012R2, 2016, 10
 SqlServer = 2012
 Mozilla = All
 "@
