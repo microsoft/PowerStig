7ca645253f7cbd4426cf1291f2b5e98ea192d663
Chase Wilson <phwilson@microsoft.com>
2018-12-12 13:55:02 -0700

    update for office data clean up

--- Module/Convert.Main/Data.Office.ps1
+++ Module/Convert.Main/Data.Office.ps1
@@ -1,65 +1,53 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
-$global:SingleLineRegistryPath += 
-     [ordered]@{
-         #     Five = [ordered]@{ Match = 'the policy value'; Select = '(?<=")(.*)(?="\sis)' };
-          Office1 = [ordered]@{
-               Match='Configure trusted add-ins';
-               Select='(.*(?<=ty\\)(.*)(?<=))'
-          }
-          Office2= [ordered]@{
-               Match='outlook\\security';
-               Select='((HKLM|HKCU).*\\security)'
-          
-          Office3= [ordered]@{
-               Match='Message Plain Format Mime';
-               Select='((?<=il\\)(.*)(?<=e\s))'
-          }
-          Office4= [ordered]@{
-               Match='the value of between';
-               Select='((?<=gs\\)(.*)(?<=Len))'
-          }
-          Office5= [ordered]@{
-               Match='FileExtensionsRemoveLevel';
-               Select='(?<=the registry value\s.)(.*)(?=.\We)'
-          }
+$global:SingleLineRegistryPath += [ordered]@{
+     Office1 = [ordered]@{
+          Match  = 'the policy value'
+          Select = '(?<=ty\\)(.*)(?<=)' 
+     }
+     Office2 = [ordered]@{
+          Match  = 'outlook\\security'; 
+          Select = '((HKLM|HKCU).*\\security)' 
+     }
+     Office3 = [ordered]@{
+          Match  = 'the value for hkcu.*Message\sPlain\sFormat\sMime'; 
+          Select = '((HKLM|HKCU).*(?=\sis))' 
+     }   
+}
+
+$global:SingleLineRegistryValueName += [ordered]@{
+     Nine     = @{
+          Match  = 'If the REG_DWORD'
+          Select = '((?<=for\s")(.*)(?<="))' 
+     } #Added for Outlook Stig - JJS
+     Ten      = @{
+          Match  = 'Message Plain Format Mime' 
+          Select = '((?<=il\\)(.*)(?<=e\s))'
+     }
+     Eleven   = @{
+          Match  = 'Configure trusted add-ins' 
+          Select = '(.*(?<=ty\\)(.*)(?<=))'
+     }
+     Twelve   = @{
+          Match  = 'the value of between'
+          Select = '((?<=gs\\)(.*)(?<=Len))'
+     }
+     Thirteen = @{
+          Match  = 'FileExtensionsRemoveLevel' 
+          Select = '(?<=the registry value\s.)(.*)(?=.\We)'
      }
 }
 
-         Office1 = [ordered]@{
-            Match = 'Configure trusted add-ins';
-            Select = '(.*(?<=ty\\)(.*)(?<=))'
-            };
-            Office2 = [ordered]@{
-            Match = 'outlook\\security';
-            Select = '((HKLM|HKCU).*\\security)'
-            };
-            Office3 = [ordered]@{
-            Match = 'Message Plain Format Mime';
-            Select = '((?<=il\\)(.*)(?<=e\s))'
-            };
-            Office4 = [ordered]@{
-            Match = 'the value of between';
-            Select = '((?<=gs\\)(.*)(?<=Len))'
-            };
-            Office5 = [ordered]@{
-            Match = 'FileExtensionsRemoveLevel';
-            Select = '(?<=the registry value\s.)(.*)(?=.\We)'
-            }
-    }
+$global:SingleLineRegistryValueType += [ordered]@{
+     Eight = @{
+          Select = '((?<=If the\s)(.*)(?<=DWORD))'
+     } #Added for Outlook Stig - JJS
+}
 
-$global:SingleLineRegistryValueName +=
-     [ordered]@{
-          Nine = @{ Match = 'If the REG_DWORD'; Select = '((?<=for\s")(.*)(?<="))'}; #Added for Outlook Stig - JJS
-          Ten = @{ Match = 'Message Plain Format Mime'; Select = '((?<=il\\)(.*)(?<=e\s))'};
-     }
-
-$global:SingleLineRegistryValueType +=
-     [ordered]@{
-     Eight = @{ Select = '((?<=If the\s)(.*)(?<=DWORD))'}; #Added for Outlook Stig - JJS
-    }
-
-$global:SingleLineRegistryValueData +=
-     [ordered]@{
-     Six = @{ Match = 'If the value PublishCalendarDetailsPolicy'; Select = '((?<=is\s)(.*)(?=\sor))'} #Added for Outlook Stig - JJS
-    }
+$global:SingleLineRegistryValueData += [ordered]@{
+     Six = @{
+          Match = 'If the value PublishCalendarDetailsPolicy' 
+          Select = '((?<=is\s)(.*)(?=\sor))'
+     } #Added for Outlook Stig - JJS
+}
+# Five = [ordered]@{ Match = 'the policy value'; Select = '(?<=")(.*)(?="\sis)' };
