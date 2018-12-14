c162d3deec31f7a2405ce5ecc36ea7f30f06774d
Jeff Salas <jesalas@microsoft.com>
2018-12-14 11:34:54 -0800

    Updated RegEx for data.office

--- Module/Convert.Main/Data.Office.ps1
+++ Module/Convert.Main/Data.Office.ps1
@@ -1,58 +1,66 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 $global:SingleLineRegistryPath += [ordered]@{
-     Office1 = [ordered]@{
+     <#Office1 = [ordered]@{
           Match  = 'the policy value'
-          Select = '(?<=ty\\)(.*)(?<=)' 
+          Select = '(?<=ty\\)(.*)(?<=)'
      }
      Office2 = [ordered]@{
-          Match  = 'outlook\\security'; 
-          Select = '((HKLM|HKCU).*\\security)' 
+          Match  = 'outlook\\security';
+          Select = '((HKLM|HKCU).*\\security)'
+     }#>
+     Office3 = [ordered]@{ #Added for Outlook Stig V-17761.b
+          Match  = 'the value for hkcu.*Message\sPlain\sFormat\sMime';
+          Select = '((HKLM|HKCU).*(?=\sis\s"))'
      }
-     Office3 = [ordered]@{
-          Match  = 'the value for hkcu.*Message\sPlain\sFormat\sMime'; 
-          Select = '((HKLM|HKCU).*(?=\sis))' 
-     }   
 }
 
 $global:SingleLineRegistryValueName += [ordered]@{
      Nine     = @{
           Match  = 'If the REG_DWORD'
-          Select = '((?<=for\s")(.*)(?<="))' 
-     } #Added for Outlook Stig - JJS
+          Select = '((?<=for\s")(.*)(?<="))'
+     }
      Ten      = @{
-          Match  = 'Message Plain Format Mime' 
+          Match  = 'Message Plain Format Mime' #Added for Outlook Stig V-17761.b
           Select = '((?<=il\\)(.*)(?<=e\s))'
      }
-     Eleven   = @{
-          Match  = 'Configure trusted add-ins' 
-          Select = '(.*(?<=ty\\)(.*)(?<=))'
+     Eleven   = @{ #Added for Outlook Stig V-17575
+          Match  = 'Configure trusted add-ins'
+          Select = '(?<=ty\\).*(?=\sIn)'
      }
-     Twelve   = @{
+     Twelve   = @{ #Added for Outlook Stig V-17761.a
           Match  = 'a value of between'
           Select = '((?<=gs\\)(.*)(?<=Len))'
      }
-     Thirteen = @{
-          Match  = 'FileExtensionsRemoveLevel' 
+     Thirteen = @{ #Added for Outlook Stig V-17774 and V-17775
+          Match  = 'FileExtensionsRemoveLevel'
           Select = '(?<=the registry value\s.)(.*)(?=.\We)'
+     }
+     Fourteen = [ordered]@{ #Added for Outlook Stig V-17733
+          Match = 'If the.+(registry key exist)'
+          Select = '(?<=ty\\).*(?=\sC)'
      }
 }
 
 $global:SingleLineRegistryValueType += [ordered]@{
      Eight = @{
           Select = '((?<=If the\s)(.*)(?<=DWORD))'
-     } #Added for Outlook Stig - JJS
+     }
+     Nine = @{
+          Select = '(?<=\sto\s).*"' #Added for Outlook Stig V-17575
+     }
 }
 
 $global:SingleLineRegistryValueData += [ordered]@{
      Six = @{
-          Match = 'If the value PublishCalendarDetailsPolicy' 
+          Match = 'If the value PublishCalendarDetailsPolicy'  #Added for Outlook Stig V-17776
           Select = '((?<=is\s)(.*)(?=\sor))'
-     } 
+     }
      Seven   = @{
-        Match  = 'a value of between'
-        Select = '(?<=between\s)(.*)(?<=\s)'
-   }
-#Added for Outlook Stig - JJS
+          Match  = 'a value of between' #Added for Outlook Stig V-17761
+          Select = '(?<=between\s)(.*)(?<=\s)'
+     }
+     Eight = @{
+          Select = '(?<=\sto\s).*"' #Added for Outlook Stig V-17575
+     }
 }
-# Five = [ordered]@{ Match = 'the policy value'; Select = '(?<=")(.*)(?="\sis)' };
