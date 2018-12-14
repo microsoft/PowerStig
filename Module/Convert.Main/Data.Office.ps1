9c5d0ce4462d9eb1982b1a1d783331fc814da175
Jeff Salas <jesalas@microsoft.com>
2018-12-14 12:16:54 -0800

    cleaned up data.office parsing rules

--- Module/Convert.Main/Data.Office.ps1
+++ Module/Convert.Main/Data.Office.ps1
@@ -1,17 +1,13 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 $global:SingleLineRegistryPath += [ordered]@{
-     <#Office1 = [ordered]@{
-          Match  = 'the policy value'
-          Select = '(?<=ty\\)(.*)(?<=)'
-     }
-     Office2 = [ordered]@{
-          Match  = 'outlook\\security';
+     Office1 = [ordered]@{
+          Match  = 'outlook\\security'
           Select = '((HKLM|HKCU).*\\security)'
-     }#>
-     Office3 = [ordered]@{ #Added for Outlook Stig V-17761.b
-          Match  = 'the value for hkcu.*Message\sPlain\sFormat\sMime';
-          Select = '((HKLM|HKCU).*(?=\sis\s"))'
+     }
+     Office2 = [ordered]@{ #Added for Outlook Stig V-17761.b
+          Match  = 'value for hkcu.*Message\sPlain\sFormat\sMime'
+          Select = '(HKCU).*(?<=me)'
      }
 }
 
@@ -20,8 +16,8 @@ $global:SingleLineRegistryValueName += [ordered]@{
           Match  = 'If the REG_DWORD'
           Select = '((?<=for\s")(.*)(?<="))'
      }
-     Ten      = @{
-          Match  = 'Message Plain Format Mime' #Added for Outlook Stig V-17761.b
+     Ten      = @{ #Added for Outlook Stig V-17761.b
+          Match  = 'Message Plain Format Mime'
           Select = '((?<=il\\)(.*)(?<=e\s))'
      }
      Eleven   = @{ #Added for Outlook Stig V-17575
@@ -46,21 +42,21 @@ $global:SingleLineRegistryValueType += [ordered]@{
      Eight = @{
           Select = '((?<=If the\s)(.*)(?<=DWORD))'
      }
-     Nine = @{
-          Select = '(?<=\sto\s).*"' #Added for Outlook Stig V-17575
+     Nine = @{ #Added for Outlook Stig V-17575
+          Select = '(?<=\sto\s).*"'
      }
 }
 
 $global:SingleLineRegistryValueData += [ordered]@{
-     Six = @{
-          Match = 'If the value PublishCalendarDetailsPolicy'  #Added for Outlook Stig V-17776
+     Six = @{ #Added for Outlook Stig V-17776
+          Match = 'If the value PublishCalendarDetailsPolicy'
           Select = '((?<=is\s)(.*)(?=\sor))'
      }
-     Seven   = @{
-          Match  = 'a value of between' #Added for Outlook Stig V-17761
+     Seven   = @{ #Added for Outlook Stig V-17761.a
+          Match  = 'a value of between'
           Select = '(?<=between\s)(.*)(?<=\s)'
      }
-     Eight = @{
-          Select = '(?<=\sto\s).*"' #Added for Outlook Stig V-17575
+     Eight = @{ #Added for Outlook Stig V-17575
+          Select = '(?<=\sto\s).*"'
      }
 }
