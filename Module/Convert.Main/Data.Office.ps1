cbb1e35bc97ce8a9ae99a963b2f6b53eb076ec87
Jeff Salas <jesalas@microsoft.com>
2018-12-26 10:12:41 -0800

    Initial Commit

--- Module/Convert.Main/Data.Office.ps1
+++ Module/Convert.Main/Data.Office.ps1
@@ -3,9 +3,9 @@
 
 <#
     Instructions:  Use this file to add/update/delete regsitry expressions that are used accross 
-    multiple technologies files that are considered commonly used.  Enure expressions are listed
+    multiple technologies files that are considered commonly used.  Ensure expressions are listed
     from MOST Restrive to LEAST Restrictive, similar to exception handling.  Also, ensure only
-    UNIQUE Keys are used in each hashtable to orevent errors and conflicts.
+    UNIQUE Keys are used in each hashtable to prevent errors and conflicts.
 #>
 
 $global:SingleLineRegistryPath += [ordered]@{
@@ -14,7 +14,7 @@ $global:SingleLineRegistryPath += [ordered]@{
         Select = '((HKLM|HKCU).*\\security)'
     }
     # Added for Outlook Stig V-17761.b
-    Office2 = [ordered]@{ 
+    Office2 = [ordered]@{
         Match  = 'value for hkcu.*Message\sPlain\sFormat\sMime'
         Select = '(HKCU).*(?<=me)'
     }
@@ -26,17 +26,17 @@ $global:SingleLineRegistryValueName += [ordered]@{
         Select = '((?<=for\s")(.*)(?<="))'
     }
     # Added for Outlook Stig V-17761.b
-    Office2 = @{ 
+    Office2 = @{
         Match  = 'Message Plain Format Mime'
         Select = '((?<=il\\)(.*)(?<=e\s))'
     }
     # Added for Outlook Stig V-17575
-    Office3 = @{ 
+    Office3 = @{
         Match  = 'Configure trusted add-ins'
         Select = '(?<=ty\\).*(?=\sIn)'
     }
     # Added for Outlook Stig V-17761.a
-    Office4 = @{ 
+    Office4 = @{
         Match  = 'a value of between'
         Select = '((?<=gs\\)(.*)(?<=Len))'
     }
@@ -46,9 +46,17 @@ $global:SingleLineRegistryValueName += [ordered]@{
         Select = '(?<=the registry value\s.)(.*)(?=.\We)'
     }
     # Added for Outlook Stig V-17733
-    Office6 = [ordered]@{ 
+    Office6 = [ordered]@{
         Match  = 'If the.+(registry key exist)'
         Select = '(?<=ty\\).*(?=\sC)'
+    }
+    # Added for Outlook 2016 Stig V-71113
+    Office7 = @{
+        Select = '(?<=If the value of\s)(.*)(?=is\s.*R)'
+    }
+    # Added for Outlook 2016 Stig V-71193
+    Office8 = @{
+        Select = '(?<=If the value\s)(.*)(?=does)'
     }
 }
 
@@ -57,19 +65,23 @@ $global:SingleLineRegistryValueType += [ordered]@{
         Select = '((?<=If the\s)(.*)(?<=DWORD))'
     }
     # Added for Outlook Stig V-17575
-    Office2 = @{ 
+    Office2 = @{
         Select = '(?<=\sto\s).*"'
     }
 }
 
 $global:SingleLineRegistryValueData += [ordered]@{
-    # Added for Outlook Stig V-17776
-    Office1 = @{ 
-        Match  = 'If the value PublishCalendarDetailsPolicy'
+    # Added for Outlook Stig 2016 V-71273
+    Office1 = @{
+        Match = 'MinEncKey'
+        Select= '((?<=or\s)(.*)(?=,))'
+    }
+    # Added for Outlook 2013 Stig V-17776 and Outlok 2016 Stig V-71133
+    Office2 = @{
         Select = '((?<=is\s)(.*)(?=\sor))'
     }
     # Added for Outlook Stig V-17761.a
-    Office2 = @{ 
+    Office3 = @{
         Match  = 'a value of between'
         Select = '(?<=between\s)(.*)(?<=\s)'
     }
