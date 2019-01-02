04916f21bd3443bfa286a723a93526ba22b107f9
nehrua <36175222+nehrua@users.noreply.github.com>
2019-01-02 15:58:50 -0500

    defender specific

---
+++ Module/Convert.Main/Data.Windows.Defender.ps1
@@ -0,0 +1,17 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+<#
+    Instructions:  Use this file to add/update/delete regsitry expressions that are used accross 
+    Windows Defender files.  Ensure expressions are listed from MOST Restrive to LEAST Restrictive, 
+    similar to exception handling.  Also, ensure only UNIQUE Keys are used in each hashtable to 
+    prevent errors and conflicts.
+#>
+
+$global:SingleLineRegistryValueData += [ordered]@{
+    # Added for Windows Defender Stig V-75237
+    Defender1 = @{
+        Match  = 'ScheduleDay'
+        Select = '(\d[x]\d)\sthrough\s(\d[x]\d)'
+    }
+}
