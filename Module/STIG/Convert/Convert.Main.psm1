dcdeb9a19db89dd0d4a651f74ba72cd2b1210aca
Adam Haynes <adamhay@microsoft.com>
2019-01-30 12:06:47 +0100

    fixed STIG tests

--- Module/STIG/Convert/Convert.Main.psm1
+++ Module/STIG/Convert/Convert.Main.psm1
@@ -1,8 +1,8 @@
 #region Header
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
-using module .\..\Common\Common.psm1
-using module .\..\Rule\Rule.psm1
+#using module .\..\Common\Common.psm1
+#using module .\..\Rule\Rule.psm1
 
 $exclude = @($MyInvocation.MyCommand.Name,'Template.*.txt', 'Data.*.ps1')
 $supportFileList = Get-ChildItem -Path $PSScriptRoot -Exclude $exclude
@@ -11,4 +11,3 @@ foreach ($supportFile in $supportFileList)
     Write-Verbose "Loading $($supportFile.FullName)"
     . $supportFile.FullName
 }
-#endregion
