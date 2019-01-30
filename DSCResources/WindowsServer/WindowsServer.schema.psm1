b5726196a48b3c75dfcc0883623596ba82e136b9
Brian Wilhite <bcwilhite@live.com>
2019-01-30 12:33:18 -0500

    Updated module manifest to reflect new AccessControlDsc 1.2.0.0 release (#254)
    * updated module manifest to reflect new AccessControlDSC release 1.2.0.0
    
    * updated composites to reflect the correct version of AccessControlDSC

--- DSCResources/WindowsServer/WindowsServer.schema.psm1
+++ DSCResources/WindowsServer/WindowsServer.schema.psm1
@@ -126,7 +126,7 @@ Configuration WindowsServer
     . "$resourcePath\windows.Script.skip.ps1"
     ##### END DO NOT MODIFY #####
 
-    Import-DscResource -ModuleName AccessControlDsc -ModuleVersion 1.1.0.0
+    Import-DscResource -ModuleName AccessControlDsc -ModuleVersion 1.2.0.0
     . "$resourcePath\windows.AccessControl.ps1"
 
     Import-DscResource -ModuleName AuditPolicyDsc -ModuleVersion 1.2.0.0
