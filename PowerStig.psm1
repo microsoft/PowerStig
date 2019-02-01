587c92b9ca43b3407094eea907320d71c6663ac0
Chase Wilson <phwilson@microsoft.com>
2019-02-01 10:28:33 -0700

    update for whitespace

--- PowerStig.psm1
+++ PowerStig.psm1
@@ -1,13 +1,13 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
-using module .\Module\Common\Common.psm1    
-using module .\Module\Stig.OrganizationalSetting\Stig.OrganizationalSetting.psm1    
-using module .\Module\Stig.SkippedRule\Stig.SkippedRule.psm1    
-using module .\Module\Stig.SkippedRuleType\Stig.SkippedRuleType.psm1    
+using module .\Module\Common\Common.psm1
+using module .\Module\Stig.OrganizationalSetting\Stig.OrganizationalSetting.psm1
+using module .\Module\Stig.SkippedRule\Stig.SkippedRule.psm1
+using module .\Module\Stig.SkippedRuleType\Stig.SkippedRuleType.psm1
 using module .\Module\STIG\STIG.psm1
-using module .\Module\Stig.StigException\Stig.StigException.psm1    
-using module .\Module\Stig.TechnologyRole\Stig.TechnologyRole.psm1    
-using module .\Module\Stig.TechnologyVersion\Stig.TechnologyVersion.psm1    
+using module .\Module\Stig.StigException\Stig.StigException.psm1
+using module .\Module\Stig.TechnologyRole\Stig.TechnologyRole.psm1
+using module .\Module\Stig.TechnologyVersion\Stig.TechnologyVersion.psm1
 
  # load the public StigData functions    
 
