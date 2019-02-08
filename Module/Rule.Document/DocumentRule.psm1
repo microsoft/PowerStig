e55b7800f887fb919a891357c9b3c6bca3b9d314
Brian Wilhite <bcwilhite@live.com>
2019-02-08 14:50:58 -0500

    Updated DocumentRule and DocumentRuleConvert Classes to parse correctly.

--- Module/Rule.Document/DocumentRule.psm1
+++ Module/Rule.Document/DocumentRule.psm1
@@ -13,6 +13,29 @@ using module .\..\Rule\Rule.psm1
 #>
 Class DocumentRule : Rule
 {
+    <#
+        .SYNOPSIS
+            Constructor that fully populates the required properties
+        .DESCRIPTION
+            Constructor that fully populates the required properties
+        .PARAMETER Id
+            The STIG ID
+        .PARAMETER Severity
+            The STIG Severity
+        .PARAMETER Title
+            The STIG Title
+        .PARAMETER RawString
+            The chcek-content element of the STIG xccdf
+    #>
+    DocumentRule ([string] $Id, [severity] $Severity, [string] $Title, [string] $RawString)
+    {
+        $this.Id = $Id
+        $this.severity = $Severity
+        $this.title = $Title
+        $this.rawString = $RawString
+        $this.DscResource = 'None'
+    }
+    
     DocumentRule () {}
 
     DocumentRule ([xml.xmlelement] $Rule, [bool] $Convert) : Base ($Rule, $Convert) {}
