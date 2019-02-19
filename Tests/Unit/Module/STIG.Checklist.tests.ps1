92407a9e58e0c804aa621947721fd455caeb6631
Adam Haynes <adamhay@microsoft.com>
2019-02-19 14:27:52 +0100

    added initial test

---
+++ Tests/Unit/Module/STIG.Checklist.tests.ps1
@@ -0,0 +1,15 @@
+. .\..\..\..\Module\STIG\Functions.Checklist.ps1
+
+
+Describe 'New-StigCheckList' {
+
+    It 'Should throw if an invalid path is provided' {
+        Mock Test-Path {return $false}
+        {New-StigCheckList -ReferenceConfiguration 'test' -XccdfPath 'test' -OutputPath 'c:\test'} | Should Throw
+    }
+
+    It 'Should throw if the full path to a .ckl file is not provided' {
+        Mock Test-Path {return $true}
+        {New-StigCheckList -ReferenceConfiguration 'test' -XccdfPath 'test' -OutputPath 'c:\test\test.ck'} | Should Throw
+    }
+}
