cd2317a2080728dd796d3e853e84fef60d540f21
Chase Wilson <phwilson@microsoft.com>
2019-05-13 10:48:03 -0700

    Update to adjust

--- Module/Rule/Convert/Functions.ps1
+++ Module/Rule/Convert/Functions.ps1
@@ -151,6 +151,7 @@ function Get-HardCodedString
         'V-26359', # Windows Server 2012R2 - Legal Banner Dialog Box Title
         'V-73649', # Windows Server 2016 - Legal Banner Dialog Box Title
         'V-17761', # Outlook 2013 - OrgSetting Value
+        'V-75237', # Windows Defender - Scan ScheduleDay
         'V-75241', # Windows Defender - ASSignatureDue
         'V-75243' # Windows Defender - AVSignatureDue
     )
@@ -211,6 +212,10 @@ function Get-HardCodedString
         {
             $hardCodedString = "'{0}' -ge '30' -and '{0}' -le '132'"
             continue
+        }
+        {$PSItem -match 'V-75237'}
+        {
+            $hardCodedString = "{0} -match '^(0x0|0x1|0x2|0x3|0x4|0x5|0x6|0x7)$'"
         }
         {$PSItem -match 'V-75241|V-75243'}
         {
