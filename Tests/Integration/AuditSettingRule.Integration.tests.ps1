91c068623533e0ca2a3b02068dd312097057d58c
Jason Walker <walkerjason@live.com>
2019-07-15 08:34:22 -0400

    Fixed tabs in tests

--- Tests/Integration/AuditSettingRule.Integration.tests.ps1
+++ Tests/Integration/AuditSettingRule.Integration.tests.ps1
@@ -11,13 +11,13 @@ try
                 desiredvalue = 'NTFS|ReFS'
                 operator     = '-match'
                 checkContent = 'Open the Computer Management Console.
-                    Expand the "Storage" object in the Tree window.
-                    Select the "Disk Management" object.
+                Expand the "Storage" object in the Tree window.
+                Select the "Disk Management" object.
 
-                    If the file system column does not indicate "NTFS" as the file system for each local hard drive, this is a finding.
+                If the file system column does not indicate "NTFS" as the file system for each local hard drive, this is a finding.
 
-                    Some hardware vendors create a small FAT partition to store troubleshooting and recovery data. No other files must be stored here.  This
-                    must be documented with the ISSO.'
+                Some hardware vendors create a small FAT partition to store troubleshooting and recovery data. No other files must be stored here.  This
+                must be documented with the ISSO.'
             },
             @{
                 query        = "SELECT * FROM Win32_OperatingSystem"
@@ -39,28 +39,28 @@ try
                 operator     = '-ge'
                 checkContent = 'Run "winver.exe".
 
-                    If the "About Windows" dialog box does not display:
+                If the "About Windows" dialog box does not display:
 
-                    "Microsoft Windows Version 1607 (OS Build 14393.0)"
+                "Microsoft Windows Version 1607 (OS Build 14393.0)"
 
-                    or greater, this is a finding.
+                or greater, this is a finding.
 
-                    Note: Microsoft has extended support for previous versions providing critical and important updates for Windows 10 Enterprise.
+                Note: Microsoft has extended support for previous versions providing critical and important updates for Windows 10 Enterprise.
 
-                    Currently supported Semi-Annual Channel versions:
-                    v1607 - Microsoft support is scheduled to end 09 April 2019.
-                    v1703 - Microsoft support is scheduled to end 08 October 2019.
-                    v1709 - Microsoft support is scheduled to end 14 April 2020.
-                    v1803 - Microsoft support is scheduled to end 10 November 2020.
-                    v1809 - Microsoft support is scheduled to end 13 April 2021.
+                Currently supported Semi-Annual Channel versions:
+                v1607 - Microsoft support is scheduled to end 09 April 2019.
+                v1703 - Microsoft support is scheduled to end 08 October 2019.
+                v1709 - Microsoft support is scheduled to end 14 April 2020.
+                v1803 - Microsoft support is scheduled to end 10 November 2020.
+                v1809 - Microsoft support is scheduled to end 13 April 2021.
 
-                    No preview versions will be used in a production environment.
+                No preview versions will be used in a production environment.
 
-                    Special purpose systems using the Long-Term Servicing Branch\Channel (LTSC\B) may be at the following versions, which are not a finding:
+                Special purpose systems using the Long-Term Servicing Branch\Channel (LTSC\B) may be at the following versions, which are not a finding:
 
-                    v1507 (Build 10240)
-                    v1607 (Build 14393)
-                    v1809 (Build 17763)'
+                v1507 (Build 10240)
+                v1607 (Build 14393)
+                v1809 (Build 17763)'
             },@{
                 query        = "SELECT * FROM Win32_OperatingSystem"
                 property     = 'Version'
