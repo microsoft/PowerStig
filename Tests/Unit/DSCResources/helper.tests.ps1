0e6b6fc9c53e6c7ce39aae3367c052051499cb34
Jason Walker <walkerjason@live.com>
2019-07-26 15:08:18 -0400

    Added tests for helper function

--- Tests/Unit/DSCResources/helper.tests.ps1
+++ Tests/Unit/DSCResources/helper.tests.ps1
@@ -49,4 +49,31 @@ Describe 'Functions' {
             $result | Should -Be '42'
         }
     }
+
+    Context 'Format-SqlScriptVariable' {
+
+        It 'Should properly format a string with 3 variables' {
+            $parameteres = @{
+                Variable      = 'TraceFilePath={0} MaxRollOverFileCount={1} MaxTraceFileSize={2}'
+                VariableValue = 'c:\trace\log,5,500'
+            }
+            $functionResult = Format-SqlScriptVariable @parameteres
+            $compareResult  = Compare-Object -ReferenceObject $functionResult -DifferenceObject @('TraceFilePath=c:\trace\log','MaxRollOverFileCount=5','MaxTraceFileSize=500')
+
+            $functionResult | Should -BeOfType 'string'
+            $compareResult  | Should -Be $null
+        }
+
+        It 'Should properly format a value with spaces' {
+            $parameters = @{
+                Variable      = 'TraceFilePath={0}'
+                VariableValue = 'C:\Program Files\Microsoft SQL Server\MSSQL11.SMA\MSSQL\Log'
+            }
+
+            $functionResult = Format-SqlScriptVariable @parameters
+
+            $functionResult | Should -BeOfType 'string'
+            $functionResult | Should -Be $($parameters.Variable -f $parameters.VariableValue)
+        }
+    }
 }
