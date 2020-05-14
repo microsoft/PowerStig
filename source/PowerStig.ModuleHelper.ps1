516b0354dae2e95cfab8ea606959120f1e1fad15
erjenkin <erjenkin@microsoft.com>
2020-05-14 14:23:14 -0400

    updated module  helper

--- source/PowerStig.ModuleHelper.ps1
+++ source/PowerStig.ModuleHelper.ps1
@@ -1,30 +1,34 @@
-#This script was created to get around a cyclic redundancy error that is present in PowerShell 5.
-#When importing PowerSTIG with the following modules listed as required modules in the manifest the error occurs
-#Link to the issue - https://github.com/PowerShell/PowerShell/issues/2607
-
-$requiredmodules = @(
-    @{ModuleName = 'VMware.VimAutomation.Sdk'; ModuleVersion = '12.0.0.15939651'}
-    @{ModuleName = 'VMware.VimAutomation.Common'; ModuleVersion = '12.0.0.15939652'}
-    @{ModuleName = 'VMware.Vim'; ModuleVersion ='7.0.0.15939650'}
-    @{ModuleName = 'VMware.VimAutomation.Cis.Core'; ModuleVersion = '12.0.0.15939657'}
-    @{ModuleName = 'VMware.VimAutomation.Core'; ModuleVersion = '12.0.0.15939655'}
-    @{ModuleName = 'VMware.VimAutomation.Storage'; ModuleVersion = '12.0.0.15939648'}
-    @{ModuleName = 'VMware.VimAutomation.Vds'; ModuleVersion = '12.0.0.15940185'}
-    @{ModuleName = 'Vmware.vSphereDsc'; ModuleVersion = '2.1.0.58'}
-)
-
+<#
+    This script was created to get around a cyclic redundancy error that is present in PowerShell 5.
+    When importing PowerSTIG with the following modules listed as required modules in the manifest the error occurs
+    Link to the issue - https://github.com/PowerShell/PowerShell/issues/2607
+#>
+$requiredModules = @{
+    'VMware.VimAutomation.Sdk' = '12.0.0.15939651'
+    'VMware.VimAutomation.Common' = '12.0.0.15939652'
+    'VMware.Vim' = '7.0.0.15939650'
+    'VMware.VimAutomation.Cis.Core' = '12.0.0.15939657'
+    'VMware.VimAutomation.Core' = '12.0.0.15939655'
+    'VMware.VimAutomation.Storage' = '12.0.0.15939648'
+    'VMware.VimAutomation.Vds' = '12.0.0.15940185'
+    'Vmware.vSphereDsc' = '2.1.0.58'
+}
 try
 {
-    foreach ($module in $requiredmodules)
+    foreach ($moduleName in $requiredModules.Keys)
     {
-        if (Get-Module -ListAvailable -Name $module.ModuleName)
+        $script:importModuleParams = @{
+            Name = $moduleName
+            Version = $requiredModules[$moduleName]
+            ErrorAction = 'Stop'
+        }
+        if (Get-Module -ListAvailable -Name $importModuleParams.Name)
         {
-            Import-Module -Name $module.ModuleName -Version $module.ModuleVersion -ErrorAction Stop
+            Import-Module @importModuleParams
         }
     }
 }
 catch
 {
-    Write-Error -Message "$($module.ModuleName) Version: $($module.ModuleVersion) is not installed, please install and try again."
+    Write-Error -Message "$($importModuleParams.Name) Version: $($importModuleParams.Version) is not installed, please install and try again."
 }
-
