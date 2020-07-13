90a7378fb7e79ce99c3eced5158429587fc8e0cc
Brian Wilhite <bcwilhite@live.com>
2020-07-13 14:30:59 -0400

    updated package tasks

--- .build/tasks/release.module.build.ps1
+++ .build/tasks/release.module.build.ps1
@@ -276,24 +276,17 @@ task package_module_nupkg {
     $projectNuspecFile = New-NuspecFile @newNuspecFileParams
     $nugetFilePath = Join-Path -Path (Get-Module -Name PSDepend -ListAvailable).ModuleBase -ChildPath 'nuget.exe' | Select-Object -Unique
     Write-Warning -Message "nuget Path: $nugetFilePath"
-    if ((Test-Path -Path $nugetFilePath) -eq $false)
-    {
-        throw "nuget.exe not found, aborting task package_module_nupkg"
+    $startProcessNugetParams = @{
+        FilePath     = $nugetFilePath
+        Wait         = $true
+        ArgumentList = @(
+            'Pack', $projectNuspecFile
+            '-OutputDirectory', $OutputDirectory
+        )
     }
-    else
-    {
-        $startProcessNugetParams = @{
-            FilePath     = $nugetFilePath
-            Wait         = $true
-            ArgumentList = @(
-                'Pack', $projectNuspecFile
-                '-OutputDirectory', $OutputDirectory
-            )
-        }
 
-        Start-Process @startProcessNugetParams
-        Write-Build Green "  Packaged $ProjectName NuGet package"
-    }
+    Start-Process @startProcessNugetParams
+    Write-Build Green "  Packaged $ProjectName NuGet package"
 
     Write-Build DarkGray "  Cleaning up"
     $null = Unregister-PSRepository -Name output -ErrorAction SilentlyContinue
