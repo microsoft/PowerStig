ff41f57dcc887ac151e02ae925c490c0bf1cd9cc
Brian Wilhite <bcwilhite@live.com>
2020-07-16 15:21:46 -0400

    updated build funct. conform to style guideline

--- .build/tasks/Custom.NugetPackage.Task.ps1
+++ .build/tasks/Custom.NugetPackage.Task.ps1
@@ -5,18 +5,23 @@ param(
     $OutputDirectory = (property OutputDirectory (Join-Path $BuildRoot 'output')),
 
     [Parameter()]
+    [string]
     $ChangelogPath = (property ChangelogPath 'CHANGELOG.md'),
 
     [Parameter()]
+    [string]
     $ReleaseNotesPath = (property ReleaseNotesPath (Join-Path $OutputDirectory 'ReleaseNotes.md')),
 
     [Parameter()]
     [string]
     $ProjectName = (property ProjectName $(
-            #Find the module manifest to deduce the Project Name
-            (Get-ChildItem $BuildRoot\*\*.psd1 -Exclude 'build.psd1', 'analyzersettings.psd1' | Where-Object {
+        #Find the module manifest to deduce the Project Name
+        (
+            Get-ChildItem $BuildRoot\*\*.psd1 -Exclude 'build.psd1', 'analyzersettings.psd1' |
+                Where-Object {
                     ($_.Directory.Name -match 'source|src' -or $_.Directory.Name -eq $_.BaseName) -and
-                    $(try
+                    $(
+                        try
                         {
                             Test-ModuleManifest $_.FullName -ErrorAction Stop
                         }
@@ -24,14 +29,17 @@ param(
                         {
                             Write-Warning $_
                             $false
-                        }) }
-            ).BaseName
-        )
+                        }
+                    )
+                }
+        ).BaseName
+    )
     ),
 
     [Parameter()]
     [string]
-    $ModuleVersion = (property ModuleVersion $(
+    $ModuleVersion = (
+        property ModuleVersion $(
             try
             {
                 (gitversion | ConvertFrom-Json -ErrorAction Stop).NuGetVersionV2
@@ -41,7 +49,8 @@ param(
                 Write-Verbose "Error attempting to use GitVersion $($_)"
                 ''
             }
-        )),
+        )
+    ),
 
     [Parameter()]
     [string]
@@ -70,28 +79,29 @@ task package_module_nupkg {
 
     # Force registering the output repository mapping to the Project's output path
     $null = Unregister-PSRepository -Name output -ErrorAction SilentlyContinue
-    $RepositoryParams = @{
+    $repositoryParams = @{
         Name            = 'output'
         SourceLocation  = $OutputDirectory
         PublishLocation = $OutputDirectory
         ErrorAction     = 'Stop'
     }
 
-    $null = Register-PSRepository @RepositoryParams
+    $null = Register-PSRepository @repositoryParams
 
     # Cleaning up existing packaged module
-    if ($ModuleToRemove = Get-ChildItem (Join-Path $OutputDirectory "$ProjectName.*.nupkg"))
+    $moduleToRemove = Get-ChildItem -Path (Join-Path -Path $OutputDirectory -ChildPath "$ProjectName.*.nupkg")
+    if ($null -ne $moduleToRemove)
     {
         Write-Build DarkGray "  Remove existing $ProjectName package"
-        Remove-Item -force -Path $ModuleToRemove -ErrorAction Stop
+        Remove-Item -Path $ModuleToRemove -Force -ErrorAction Stop
     }
 
     # find Module manifest
-    $BuiltModuleManifest = (Get-ChildItem (Join-Path $OutputDirectory $ProjectName) -Depth 2 -Filter "$ProjectName.psd1").FullName |
+    $builtModuleManifest = (Get-ChildItem -Path (Join-Path -Path $OutputDirectory -ChildPath $ProjectName) -Depth 2 -Filter "$ProjectName.psd1").FullName |
         Where-Object {
             try
             {
-                Test-ModuleManifest -ErrorAction Stop -Path $_
+                Test-ModuleManifest -Path $_ -ErrorAction Stop
             }
             catch
             {
@@ -99,32 +109,38 @@ task package_module_nupkg {
             }
         }
 
-    if (-not $BuiltModuleManifest)
+    if (-not $builtModuleManifest)
     {
         throw "No valid manifest found for project $ProjectName."
     }
-    Write-Build DarkGray "  Built module's Manifest found at $BuiltModuleManifest"
+
+    Write-Build -Color DarkGray -Text "  Built module's Manifest found at $builtModuleManifest"
 
     # load module manifest
-    $ModuleInfo = Import-PowerShellDataFile -Path $BuiltModuleManifest
+    $moduleInfo = Import-PowerShellDataFile -Path $builtModuleManifest
 
     # Publish dependencies (from environment) so we can publish the built module
-    foreach ($module in $ModuleInfo.RequiredModules)
+    foreach ($module in $moduleInfo.RequiredModules)
     {
-        if (!([Microsoft.PowerShell.Commands.ModuleSpecification]$module | Find-Module -repository output -ErrorAction SilentlyContinue))
+        if (
+            -not ([Microsoft.PowerShell.Commands.ModuleSpecification]$module |
+                Find-Module -Repository output -ErrorAction SilentlyContinue)
+        )
         {
             # Replace the module by first (path & version) resolved in PSModulePath
             $module = Get-Module -ListAvailable -FullyQualifiedName $module | Select-Object -First 1
-            if ($Prerelease = $module.PrivateData.PSData.Prerelease)
+            if ($null -ne $module.PrivateData.PSData.Prerelease)
             {
-                $Prerelease = "-" + $Prerelease
+                $Prerelease = "-" + $module.PrivateData.PSData.Prerelease
             }
-            Write-Build Yellow ("  Packaging Required Module {0} v{1}{2}" -f $Module.Name, $Module.Version.ToString(), $Prerelease)
-            Publish-Module -Repository output -ErrorAction SilentlyContinue -Path $module.ModuleBase
+
+            $writeBuildText = "  Packaging Required Module {0} v{1}{2}" -f $Module.Name, $Module.Version.ToString(), $Prerelease
+            Write-Build -Color Yellow -Text $writeBuildText
+            Publish-Module -Repository output -Path $module.ModuleBase -ErrorAction SilentlyContinue
         }
     }
 
-    Write-Build DarkGray "  Creating nuspec file"
+    Write-Build -Color DarkGray -Text "  Creating nuspec file"
     $projectPath = Join-Path -Path $OutputDirectory -ChildPath $ProjectName
     $manifestFileName = '{0}.psd1' -f $ProjectName
     $moduleManifestPath = Get-ChildItem -Path $projectPath -Filter $manifestFileName -Recurse
@@ -135,7 +151,7 @@ task package_module_nupkg {
     $projectNuspecFile = New-NuspecFile @newNuspecFileParams
     $nugetResults = Get-Command -Name nuget.exe | Select-Object -First 1
     $nugetFilePath = $nugetResults.Source
-    Write-Build DarkGray "  nuget Path: $($nugetFilePath)"
+    Write-Build -Color DarkGray -Text "  nuget Path: $($nugetFilePath)"
     if ((Test-Path -Path $nugetFilePath) -eq $false)
     {
         throw "nuget.exe not found, aborting task package_module_nupkg"
@@ -152,9 +168,9 @@ task package_module_nupkg {
         }
 
         Start-Process @startProcessNugetParams
-        Write-Build Green "  Packaged $ProjectName NuGet package"
+        Write-Build -Color Green -Text "  Packaged $ProjectName NuGet package"
     }
 
-    Write-Build DarkGray "  Cleaning up"
+    Write-Build -Color DarkGray -Text "  Cleaning up"
     $null = Unregister-PSRepository -Name output -ErrorAction SilentlyContinue
 }
