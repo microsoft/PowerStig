1d06693b81465075170f5478bafa4f1f27881732
Brian Wilhite <bcwilhite@live.com>
2020-08-03 12:13:53 -0400

    updated composites with AccessControlDsc 1.4.1

--- source/DSCResources/SqlServer/SqlServer.schema.psm1
+++ source/DSCResources/SqlServer/SqlServer.schema.psm1
@@ -98,7 +98,7 @@ configuration SqlServer
     Import-DscResource -ModuleName SecurityPolicyDsc -ModuleVersion 2.4.0.0
     . "$resourcePath\Windows.SecurityOption.ps1"
 
-    Import-DscResource -ModuleName AccessControlDsc -ModuleVersion 1.4.0.0
+    Import-DscResource -ModuleName AccessControlDsc -ModuleVersion 1.4.1
     . "$resourcePath\Windows.AccessControl.ps1"
 
     Import-DscResource -ModuleName PSDscResources -ModuleVersion 2.10.0.0
