a22060d413775fb37d44698dc0e9ac5236612b2e
Christopher Weaver <christwe@microsoft.com>
2020-09-14 16:41:31 -0400

    Updating files to align with ejm5204#602 branch

---
+++ source/Module/Rule.CipherSuites/Convert/Methods.ps1
@@ -0,0 +1,38 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+#region Method Functions
+
+<#
+    .SYNOPSIS
+        Checks the string for text that indicates a range of acceptable
+        acceptable values are allowed by the STIG.
+#>
+function Test-OrgRuleRange
+{
+    [CmdletBinding()]
+    [OutputType([bool])]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [AllowEmptyString()]
+        [string[]]
+        $CheckContent
+    )
+
+    Write-Verbose "[$($MyInvocation.MyCommand.Name)]"
+    $string = $CheckContent
+    #$string = Get-SecurityPolicyString -CheckContent $checkContent
+    #$string = Get-TestStringTokenList -String $string
+
+    # This array is joined into an or '|' before being evaluated
+    $matchList = @('.*(DES or RC4).*(?:this is a finding\.)')
+
+    if ( $string -match ($matchList -join '|') )
+    {
+        return $true
+    }
+
+    return $false
+}
+
+#endregion
