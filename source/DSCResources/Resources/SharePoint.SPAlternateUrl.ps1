c60bfd82fb7fec69765e12cedcb8576e11ed807f
Christopher Weaver <christwe@microsoft.com>
2020-09-29 13:12:23 -0400

    Fixed issue with Internal in SPAlternateUrl

--- source/DSCResources/Resources/SharePoint.SPAlternateUrl.ps1
+++ source/DSCResources/Resources/SharePoint.SPAlternateUrl.ps1
@@ -5,12 +5,19 @@ $rules = Select-Rule -Type SPAlternateUrlRule -RuleList $stig.RuleList
 
 foreach ($rule in $rules)
 {  
+    if ($SPAlternateUrlItem['Internal'] -like '*true' -or $SPAlternateUrlItem['Internal'] -eq 1)
+    {
+        [bool] $internal = $true
+    }
+    else {
+        [bool] $internal = $false
+    }
     SPAlternateUrl (Get-ResourceTitle -Rule $rule)
     {
         Url                     = $SPAlternateUrlItem['Url']
         Zone                    = $SPAlternateUrlItem['Zone']
         WebAppName              = $SPAlternateUrlItem['WebAppName']
-        Internal                = $SPAlternateUrlItem['Internal']
+        Internal                = $internal
         Ensure                  = "Present"
         PsDscRunAsCredential    = $SetupAccount
     }
