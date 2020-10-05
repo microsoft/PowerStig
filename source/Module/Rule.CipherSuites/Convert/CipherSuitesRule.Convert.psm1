0bbade4a01b4568cdf73373030a4f45bd30e782e
Chris W <christwe@microsoft.com>
2020-10-05 17:12:44 -0400

    Removed unused code

--- source/Module/Rule.CipherSuites/Convert/CipherSuitesRule.Convert.psm1
+++ source/Module/Rule.CipherSuites/Convert/CipherSuitesRule.Convert.psm1
@@ -40,69 +40,11 @@ class CipherSuitesRuleConvert : CipherSuitesRule
     #>
     CipherSuitesRuleConvert ([xml.xmlelement] $XccdfRule) : base ($XccdfRule, $true)
     {
-       # $this.SetCipherSuitesOrder()
         $this.SetDuplicateRule()
         $this.SetDscResource()
     }
 
     #region Methods
-
-    <#
-        .SYNOPSIS
-            Sets the Security Option name that was extracted from the xccdf.
-        .DESCRIPTION
-            Gets the Security Option name token from the regular expression match
-            group and sets the policy Name. If the named group is null, the
-            convert status is set to fail.
-    #>
-    [void] SetCipherSuitesOrder ()
-    {
-        if ($this.OrgRuleContainsRange())
-        {
-            $this.SetOrganizationValue()
-        }
-    }
-
-    <#
-        .SYNOPSIS
-            Looks for a range of values defined in the rule.
-        .DESCRIPTION
-            A regular expression is applied to the rule to look for key words
-            and sentence structures that define a list of valid values. If a
-            range is detected the test returns true and false if not.
-    #>
-    [bool] OrgRuleContainsRange ()
-    {
-        if (Test-OrgRuleRange -CheckContent $this.SplitCheckContent)
-        {
-            return $true
-        }
-
-        return $false
-    }
-    
-    <#
-        .SYNOPSIS
-            Sets the organizational value with the correct range.
-        .DESCRIPTION
-            The range of valid values is enforced in the organizational settings
-            with a PowerShell expression. The range of values are extracted and
-            converted into a PS expression that is evaluated when the rule is
-            loaded. For example, if a value is allowed to be between 1 and 3,
-            the user provided org setting will be evaluated to ensure that they
-            are within policy guide lines and throw an error if not.
-    #>
-    [void] SetOrganizationValue ()
-    {
-        $this.set_OrganizationValueRequired($true)
-
-        $thisValueTestString = "'{0}' 'must be an array of cipher suites that are not DES or RC4'"
-
-        if (-not $this.SetStatus($thisValueTestString))
-        {
-            $this.set_OrganizationValueTestString($thisValueTestString)
-        }
-    }
 
     hidden [void] SetDscResource ()
     {
