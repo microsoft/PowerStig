1a812c139ae160e4060c3da13c9670617a0b5624
unknown <erjenkin@microsoft.com>
2020-12-21 17:16:33 -0500

    removed half legal notices from old style Hardcoded

--- source/Module/Rule/Convert/Functions.ps1
+++ source/Module/Rule/Convert/Functions.ps1
@@ -50,10 +50,6 @@ function Test-ValueDataIsHardCoded
         'V-205631', # Windows Server 2019 - Legal Notice Display
         'V-63675', # Windows Client - Legal Notice Display
         'V-220921', # Windows Client - Legal Notice Display
-        'V-26359', # Windows Server 2012R2 - Legal Banner Dialog Box Title
-        'V-225466', # Windows Server 2012R2 (MS) - Legal Banner Dialog Box Title
-        'V-226289', # Windows Server 2012R2 (DC) - Legal Banner Dialog Box Title
-        'V-73649', # Windows Server 2016 - Legal Banner Dialog Box Title
         'V-225037', # Windows Server 2016 - Legal Banner Dialog Box Title
         'V-93149', # Windows Server 2019 - Legal Banner Dialog Box Title
         'V-205632', # Windows Server 2019 - Legal Banner Dialog Box Title
@@ -98,7 +94,7 @@ function Get-HardCodedString
             Write-Verbose -Message "[$($MyInvocation.MyCommand.Name)] LegalNotice : $true"
             return $script:legalNoticeText
         }
-        {$PSItem -match 'V-26359|V-63681|V-73649|V-93149|V-225466|V-226289|V-225037|V-220922'}
+        {$PSItem -match 'V-63681|V-93149|V-225037|V-220922'}
         {
             Write-Verbose -Message "[$($MyInvocation.MyCommand.Name)] LegalCaption : $true"
             return $script:legalNoticeCaption
@@ -127,10 +123,6 @@ function Get-HardCodedString
 
     $stigIds = @(
 
-        'V-26359', # Windows Server 2012R2 - Legal Banner Dialog Box Title
-        'V-225466', # Windows Server 2012R2 (MS) - Legal Banner Dialog Box Title
-        'V-226289', # Windows Server 2012R2 (DC) - Legal Banner Dialog Box Title
-        'V-73649', # Windows Server 2016 - Legal Banner Dialog Box Title
         'V-225037', # Windows Server 2016 - Legal Banner Dialog Box Title
         'V-93149', # Windows Server 2019 - Legal Banner Dialog Box Title
         'V-205632', # Windows Server 2019 - Legal Banner Dialog Box Title
@@ -170,7 +162,7 @@ function Get-HardCodedString
 
     switch ($stigId)
     {
-        {$PSItem -match 'V-26359|V-73649|V-93149|V-63681|V-225466|V-226289|V-205632|V-220922|V-225037'}
+        {$PSItem -match 'V-93149|V-63681|V-205632|V-220922|V-225037'}
         {
             $hardCodedString = "'{0}' -match '^(DoD Notice and Consent Banner|US Department of Defense Warning Statement)$'"
             continue
