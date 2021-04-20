5c0c4b19e34dc9083d398f24029a6c5125c67b37
Adam Davenport <addavenp@microsoft.com>
2021-04-19 20:03:39 -0500

    SQLServerConfiguration : Made changes to follow style guidelines

--- source/DSCResources/Resources/SqlServer.SQLServerConfiguration.ps1
+++ source/DSCResources/Resources/SqlServer.SQLServerConfiguration.ps1
@@ -3,54 +3,28 @@
 
 $Rules = $Stig.RuleList | Select-Rule -Type SqlServerDSCRule
 
-    #Sets variables for Default and Named Instances.
-    foreach ($Instance in $ServerInstance)
+# Sets variables for Default and Named Instances.
+foreach ($instance in $serverInstance)
+{
+    if ($instance -notmatch '\\')
     {
-        Try{
-            If($Instance -notmatch "\\"){
-                $InstanceName = "MSSQLSERVER"
-                $ServerName = $Instance
-                #$SQLConnectionName = $HostName
-        }
-            Else{
-                $InstanceName = $Instance.Split("{\}")[1]
-                $ServerName = $Instance.Split("{\}")[0]
-                #$SQLConnectionName = $SQLInstanceName
-            }
-    }
-    Catch
+        $instanceName = 'MSSQLSERVER'
+        $serverName = $instance
+    } 
+    else
     {
-        $ErrorMessage = $_.Exception.Message
-        Write-Verbose $ErrorMessage
-    }
+        $instanceName = $instance.Split('{\}')[1]
+        $serverName = $instance.Split('{\}')[0]
+   }
 
-        foreach ($rule in $rules)
+    foreach ($rule in $rules)
+    {
+        SqlServerConfiguration (Get-ResourceTitle -Rule $rule)
         {
-            Try{
-                #XML has placeholder keywords. This replaces placeholder keywords with actual values for configuration.
-                #$InstanceName = $Rule.InstanceName
-                #$InstanceName = ($InstanceName) | ForEach-Object{$_.replace('SQLInstanceName',$SQLInstanceName)}
-
-                #$ServerName = $Rule.ServerName
-                #$ServerName = ($ServerName) | ForEach-Object{$_.replace('HostName',$HostName)}
-            
-                #Creates correct format for Named Instances.    
-                #If($SQLInstanceName -match "\\"){
-                #    $InstanceName = $InstanceName.Split("{\}")[1]
-                #    $InstanceName = ($InstanceName) | ForEach-Object{$_.replace('SQLInstanceName',$SQLInstanceName)}
-                #}
-
-                SqlServerConfiguration (Get-ResourceTitle -Rule $rule)
-                {
-                    InstanceName = $InstanceName
-                    ServerName = $ServerName
-                    OptionName = $Rule.OptionName
-                    OptionValue = $Rule.OptionValue
-                }
-            }
-            Catch{
-                $ErrorMessage = $_.Exception.Message
-                Write-Verbose $ErrorMessage
-            }
+            InstanceName = $instanceName
+            ServerName = $serverName
+            OptionName = $rule.OptionName
+            OptionValue = $rule.OptionValue
         }
     }
+}
