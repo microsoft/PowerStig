2143e7c2d5b13f02de9ed528ca9c01e3a8e107b7
mcollera <matthewcollera@outlook.com>
2024-01-10 11:30:46 -0500

    Update FireFox.schema to include registry stigs

--- source/DSCResources/FireFox/FireFox.schema.psm1
+++ source/DSCResources/FireFox/FireFox.schema.psm1
@@ -72,8 +72,11 @@ configuration FireFox
     ##### END DO NOT MODIFY #####
 
     Import-DscResource -ModuleName FileContentDsc -ModuleVersion 1.3.0.151
-    . "$resourcePath\firefox.ReplaceText.ps1"
-
     Import-DscResource -ModuleName PSDSCresources -ModuleVersion 2.12.0.0
+    Import-DscResource -ModuleName GPRegistryPolicyDsc -ModuleVersion 1.2.0
+
+    . "$resourcePath\firefox.ReplaceText.ps1"
     . "$resourcePath\windows.Script.skip.ps1"
+    . "$resourcePath\windows.Registry.ps1"
+    . "$resourcePath\windows.RefreshRegistryPolicy.ps1"
 }
